53cfccc6f0ed94fa8e76efa3e6920838e07a065c
asaad <asaad@MINPC>
2022-03-20 19:26:45 +0100

    Add project files.

---
+++ ENNEM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ENNEM.MvcApplication" Language="C#" %>
---
+++ ENNEM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ENNEM.Startup))]
+namespace ENNEM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
